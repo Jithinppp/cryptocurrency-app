6e8f65805ea5a2040c79eac92048f0ceedf1f947
Jithinppp <jithinp968@gmail.com>
2022-01-26 18:57:04 +0530

    the boiler plate of the react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    axios.get("https://api.coinstats.app/public/v1/coins?skip=10");
+  }, []);
+
+  return <div className="App">hello world</div>;
 }
 
 export default App;
